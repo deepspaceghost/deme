f113688395a67f1976d279aeba44c38e56dfade7
Hemingway <ghostblac@protonmail.com>
2021-04-04 23:48:17 -0400

    Added a when_member_joins function.
    Deme now has the capacity to welcome new members to the Discord server. Fixed an issue regarding importing the guild name from the .env file. Minor fixes include correcting typos and improving readability.

--- deme.py
+++ deme.py
@@ -5,17 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 client = discord.Client()
 
 
 @client.event
-async def on_ready():
+async def when_ready():
     """
-    Handles the event when the Client has established a connection to Discord and it has finished
-    preparing the data Discord has sent.
+    Handles what happens when the Client has established a connection to Discord and it has
+    finished preparing the data Discord has sent. It will be called (and print its messages) once
+    client is ready for further action.
     """
 
-    guild = discord.utils.get(client.guilds, name="DISCORD_GUILD")
+    guild = discord.utils.get(client.guilds, name="Artinov")
 
     print(
         f"{client.user} is connected to the following guild:\n"
@@ -24,5 +26,17 @@ async def on_ready():
 
     members = "\n - ".join([member.name for member in guild.members])
     print(f"Guild Members:\n - {members}")
+
+
+@client.event
+async def when_member_joins(member):
+    """
+    Handles what happens when a new member joins a guild.
+    """
+
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Hi {member.name}, welcome to Artinov!"
+    )
 
 client.run(TOKEN)
