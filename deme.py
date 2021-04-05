44da4d90aa777cd53839216d9c68030f464dbb37
Hemingway <ghostblac@protonmail.com>
2021-04-05 03:26:06 -0400

    Added a on_message function.
    Deme now has the capacity to send Mr. Robot quotes, upon request. Fixed an issue regarding calling functions. Minor fixes include correcting typos and improving readability.

--- deme.py
+++ deme.py
@@ -1,42 +1,58 @@
 import discord
 import os
+import random
 
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
 client = discord.Client()
 
 
 @client.event
-async def when_ready():
+async def on_ready():
     """
-    Handles what happens when the Client has established a connection to Discord and it has
-    finished preparing the data Discord has sent. It will be called (and print its messages) once
-    client is ready for further action.
+    Handles what happens when the Client has established a connection to Discord.
     """
-
-    guild = discord.utils.get(client.guilds, name="DISCORD_GUILD")
 
     print(
-        f"{client.user} is connected to the following guild:\n"
-        f"{guild.name}(id: {guild.id})\n"
+        f"{client.user.name} is connected to Discord."
     )
-
-    members = "\n - ".join([member.name for member in guild.members])
-    print(f"Guild Members:\n - {members}")
 
 
 @client.event
 async def when_member_joins(member):
     """
-    Handles what happens when a new member joins a guild.
+    Handles what happens when a new member joins a server.
     """
 
     await member.create_dm()
     await member.dm_channel.send(
-        f"Hi {member.name}, welcome to Artinov!"
+        f"Hi {member.name}, welcome to the server!"
     )
+
+
+@client.event
+async def on_message(message):
+    """
+    Handles what happens when a message is posted in a channel Deme has access to.
+    """
+
+    if message.author == client.user:
+        return
+
+    mr_robot_quotes = [
+        "We're all living in each other's paranoia.",
+        "I wanted to save the world.",
+        "No rest for the wicked.",
+        "Exciting time in the world right now. Exciting time.",
+        (
+            "I never want to be right about my hacks, but people always find a way to disappoint."
+        ),
+    ]
+
+    if message.content == "e1110t!":
+        response = random.choice(mr_robot_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
