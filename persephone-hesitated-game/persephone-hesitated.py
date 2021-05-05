a36f263021ca936fd2ccece3e887f17c26609ddd
Hemingway <ghostblac@protonmail.com>
2021-05-05 12:03:41 -0400

    Added the persephone-hesitated.py file.

---
+++ persephone-hesitated-game/persephone-hesitated.py
@@ -0,0 +1,355 @@
+import datetime
+from time import sleep
+
+opening = [
+    "PERSEPHONE HESITATED",  # all
+    "© 2021 demiurge"  # iv
+]
+
+for i in range(2):
+    print(opening[i])
+    sleep(133)
+
+# chapter_one = [
+#     "1: recognition",
+# ]
+
+name = input("What is your name?: ")
+
+if name != "Niobe" or name != "Morpheus":
+	print("Not as memorable as Niobe or Morpheus, is it?")
+
+persephone = [
+    f"Wake up, {name}..."
+]
+
+for i in range(1):
+    print(persephone[i])
+    sleep(11.25)
+
+persephone2 = [
+    "I have you..."
+]
+
+for i in range(1):
+    print(persephone2[i])
+    sleep(11.25)
+
+persephone3 = [
+    "Follow the yellow sign."
+]
+
+for i in range(1):
+    print(persephone3[i])
+    sleep(11.25)
+
+persephone4 = [
+    f"Check your messages, {name}."
+]
+
+for i in range(1):
+    print(persephone4[i])
+    sleep(11.25)
+
+# routing: 603432324
+
+# account: 61946872427
+
+choice1 = input("Check incoming message? (y / n / ls): ")
+
+if choice1 == "y":
+    
+    yes = [
+        "knowsbiercing42: Persephone?",
+        "knowsbiercing42: For some reason my first thought was Bellucci's character from The Matrix sequels.",
+        "knowsbeircing42: Hmm, strange text on your computer, could still be Bellucci.",
+        "knowsbeircing42: Makes sense to me."
+    ]
+
+    for i in range(4):
+    	print(yes[i])
+    	sleep(6.1)
+
+    # question2
+    #     if choice3 == "a":
+    #         question4
+    #             if choice7
+    #                 question8
+    #                     if choice15
+    #                         question16
+    #                             if choice31
+    #                                 question32
+    #                                     if choice31
+    #                                         ending1
+    #                                     if choice32
+    #                                         ending2
+    #                             if choice32
+    #                                 question33
+    #                                     if choice33
+    #                                         ending3
+    #                                     if choice34
+    #                                         ending4
+    #                     if choice16
+    #                         question17
+    #                             if choice33
+    #                                 question34
+    #                                     if choice35
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+
+    #                             if choice34
+    #                                 question35
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #             if choice8
+    #                 question9
+    #                     if choice17
+    #                         question18
+    #                             if choice35
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice36
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                     if choice18
+    #                         question19
+    #                             if choice37
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice38
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #     elif choice4 == "b":
+    #         question5
+    #             if chpice9
+    #                 question10
+    #                     if choice19
+    #                         question20
+    #                             if choice39
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice40
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                     if choice20
+    #                         question21
+    #                             if choice41
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice42
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #             if choice10
+    #                 question11
+    #                     if choice21
+    #                         question22
+    #                             if choice43
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice44
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                     if choice22
+    #                         question23
+    #                             if choice45
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice46
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #     else:
+    #         # default to either a or b at random
+
+elif choice1 == "ls":
+    
+    list_files = [
+        "README.md",
+        "v1.8.8.61225_release_notes"
+    ]
+
+    for i in range(2):
+    	print(list_files[i])
+
+    # question2
+    #     if choice3 == "a":
+    #         question4
+    #             if choice7
+    #                 question8
+    #                     if choice15
+    #                         question16
+    #                             if choice31
+    #                                 question32
+    #                                     if choice31
+    #                                         ending1
+    #                                     if choice32
+    #                                         ending2
+    #                             if choice32
+    #                                 question33
+    #                                     if choice33
+    #                                         ending3
+    #                                     if choice34
+    #                                         ending4
+    #                     if choice16
+    #                         question17
+    #                             if choice33
+    #                                 question34
+    #                                     if choice35
+    #                                          ending
+    #                                     if choice
+    #                                         ending
+
+    #                             if choice34
+    #                                 question35
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #             if choice8
+    #                 question9
+    #                     if choice17
+    #                         question18
+    #                             if choice35
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice36
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                     if choice18
+    #                         question19
+    #                             if choice37
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice38
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #     elif choice4 == "b":
+    #         question5
+    #             if chpice9
+    #                 question10
+    #                     if choice19
+    #                         question20
+    #                             if choice39
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice40
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                     if choice20
+    #                         question21
+    #                             if choice41
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice42
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #             if choice10
+    #                 question11
+    #                     if choice21
+    #                         question22
+    #                             if choice43
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice44
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                     if choice22
+    #                         question23
+    #                             if choice45
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #                             if choice46
+    #                                 question
+    #                                     if choice
+    #                                         ending
+    #                                     if choice
+    #                                         ending
+    #     else:
+    #         default to either a or b at random
+
+elif choice1 == "no":
+	print("You unplugged your computer and went for a walk.")
+
+else:
+	pass
