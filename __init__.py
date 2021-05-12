e300d501f6deaba0bfd60c418ce3944c48d0c920
Hemingway <ghostblac@protonmail.com>
2021-05-12 17:34:37 -0400

    Added commerrors package files.
    ADDITIONS: Added the comerrors package, made up of the __init__.py, __version__.py, food_and_cooking.txt, and main.py files.

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .__version__ import __title__, __description__, __url__, __version__, __author__, __author_email__, __license__, __copyright__
+from commerrors.main import get_error, food_and_cooking_errors, all_errors
