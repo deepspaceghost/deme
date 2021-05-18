9fc9ed5e5cf71db3d24a58fba302cb50e3c4b0e0
Hemingway <ghostblac@protonmail.com>
2021-05-18 13:53:47 -0400

    Added additional files to the package.

---
+++ prousthon/__init__.py
@@ -0,0 +1,2 @@
+from .__version__ import __title__, __description__, __url__, __version__, __author__, __author_email__, __license__, __copyright__
+from prousthon.main import get_question, proust_questions, istudio_questions, vanity_questions, all_questions
---
+++ prousthon/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "prousthon"
+__description__ = "Package to generate Proust-related questions"
+__url__ = "https://github.com/deepspaceghost/deme-app/tree/prousthon"
+__version__ = "0.0.0.150"
+__author__ = "deepspaceghost"
+__author_email__ = "ghostblac@protonmail.com"
+__license__ = "none"
+__copyright__ = "© 2021 demiurge"
