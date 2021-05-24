037f37327aa069265ae8f5d49a951f215ffe14c0
Hemingway <ghostblac@protonmail.com>
2021-05-24 17:23:10 -0400

    Add files via upload

---
+++ persephone-hesitated-game/untitled.py
@@ -0,0 +1,16 @@
+"1886, An Inhabitant in Carcosa",
+"1891, Haïta the Shepherd",
+"1895, The Repairer of Reputations",
+"1895, The Mask",
+"1895, In the Court of the Dragons",
+"1895, The Yellow Sign"
+
+Demeter + Persephone myth
+
+"Where Time Shadows" + "Federal Bureau of Control in the Oldest House"
+
+1 player character
+4 AIs
+28 NPCs / enemies
+
+Automatons / Genshin Impact
